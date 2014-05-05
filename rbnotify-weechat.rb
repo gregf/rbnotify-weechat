5eef7e070bfabc19e32cc86ebf0f27802ec9484b
Greg Fitzgerald <greg@gregf.org>
2014-05-05 14:45:22 -0400

    Rework the script to call notify-send rather than using a library.
    I have come to release using gems with weechat breaks way to often
    on my system, and I just wanted something that works without the
    fuss.

--- rbnotify-weechat.rb
+++ rbnotify-weechat.rb
@@ -1,51 +1,69 @@
-begin
-  require 'libnotify'
-rescue
-  require 'rubygems'
-  require 'libnotify'
-end
+#!/usr/bin/env ruby
 
 SCRIPT_NAME = 'rbnotify-weechat'
-SCRIPT_AUTHOR = 'Greg Fitzgerald <netzdamon@gmail.com'
+SCRIPT_AUTHOR = 'Greg Fitzgerald <greg@gregf.org>'
 SCRIPT_DESC = 'Sends libnotify notifications upon events.'
-SCRIPT_VERSION = '0.1'
+SCRIPT_VERSION = '0.2'
 SCRIPT_LICENSE = 'MIT'
+
+DEFAULTS = {
+  'show_private_message' => 'on',
+  'show_highlight' => 'on'
+}
+
 
 def weechat_init
   Weechat.register SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""
+  DEFAULTS.each_pair { |option, def_value|
+    cur_value = Weechat.config_get_plugin(option)
+    if cur_value.nil? || cur_value.empty?
+      Weechat.config_set_plugin(option, def_value)
+    end
+  }
 
-  Weechat.hook_signal("weechat_highlight", "notify", "")
-  Weechat.hook_signal("weechat_pv", "notify", "")
+  hook_notifications
 
   return Weechat::WEECHAT_RC_OK
 end
 
-def notify(data, signal, signal_data)
+def hook_notifications
+  Weechat.hook_signal("weechat_pv", "show_private", "")
+  Weechat.hook_signal("weechat_highlight", "show_highlight", "")
+end
 
-  if signal == "weechat_pv"
-    event = "Private message"
-  elsif signal == "weechat_highlight"
-    event = "Highlight"
+def unhook_notifications(data, signal, message)
+  Weechat.unhook(show_private)
+  Weechat.unhook(show_highlight)
+end
+
+def show_private(data, signal, message)
+  if Weechat.config_get_plugin('show_private_message') == 'on'
+    message[0..1] == '--' ? sticky = false : sticky = true
+    show_notification("Private", "Weechat Private Message",  message, sticky)
+    return Weechat::WEECHAT_RC_OK
   end
+end
 
-  n = Libnotify.new do |notify|
-    notify.summary    = event
-    notify.body       = signal_data
-    notify.timeout    = 8      # 1.5 (s), 1000 (ms), "2", nil, false
-    notify.urgency    = :low   # :low, :normal, :critical
-    notify.append     = true   # default true - append onto existing notification
-    notify.transient  = true   # default false - keep the notifications around after display
-    notify.icon_path  = "icon.png"
-   end
-   n.show!
+def show_highlight(data, signal, message)
+ if Weechat.config_get_plugin('show_highlight') == 'on'
+    message[0..1] == '--' ? sticky = false : sticky = true
+    show_notification("Highlight", "Weechat",  message, sticky)
+    return Weechat::WEECHAT_RC_OK
+  end
+end
 
-  return Weechat::WEECHAT_RC_OK
+def show_notification(name, title, message, sticky = true)
+  if sticky
+    system("/usr/bin/notify-send -u critical '#{name} ' '#{message}'")
+  else
+    system("/usr/bin/notify-send -u low '#{name} ' '#{message}'")
+  end
 end
 
 __END__
 __LICENSE__
 
-Copyright (c) 2011 Greg Fitzgerald <netzdamon@gmail.com>
+Copyright (c) 2014 Greg Fitzgerald <greg@gregf.org>
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
@@ -67,4 +85,3 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-
